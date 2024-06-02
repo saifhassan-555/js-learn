df4dd9def86660d3d85c55c638f076b1a6aa8dc6
Saif Hassan <saifhassan451@gmail.com>
2024-06-02 11:32:34 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3 + 3) we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high 
+    // Two Important Documentation tc39 & mdn mozilla
+
+console.log("saif")
+
+let name ="saif"
+let age = 23
+let isLoggedIn = false
+let state;
+
+// premitive datatypes
+// number=> 2 to the power 53
+// bigint
+// string =>""
+// boolean = true/false
+// null=> standalone value
+// undefined=>
+// symbols=> unique  
+
+
+//object
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
