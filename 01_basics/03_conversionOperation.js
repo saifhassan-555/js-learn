07300a587eee09de79c1265378ec06654fccc567
Saif Hassan <saifhassan451@gmail.com>
2024-06-03 09:14:56 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -13,7 +13,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 //1 => true; 0=> false
 //""=> false
@@ -21,5 +21,41 @@ console.log(booleanIsLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//*********************Operation*******************//
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**3)
+// console.log(2%3)
+
+let str1 = "Hello"
+let str2 = " Saif"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+2)
+console.log(1+2+"2");
+console.log(1+2+"2");
+
+console.log((3+4) * 5 %3); //best way to write in brackets
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+//++gameCounter;
+console.log(gameCounter);
+
