4346eee80e4a73915681261d0545f4cad30977b1
Saif Hassan <saifhassan451@gmail.com>
2024-07-14 11:33:24 +0000

    oop

--- 10_classes_and_oop/oop.js
+++ 10_classes_and_oop/oop.js
@@ -17,9 +17,26 @@ console.log(this); // output will be empty paranthesis
 
 //Contructor Function
 
-const promiseOne = new Promise() 
+// const promiseOne = new Promise() 
 const date = new Date() // new keyword is contructor function it allow to create multiple instances by one object literal
 
 function User(username, loginCount, isLoggedIn){
-   this.username = username
+   this.username =   username
+   this.loginCount = loginCount
+   this.isLoggedIn = isLoggedIn
+
+   this. greetings = function(){
+    //  console.log(`Welcome ${this.username}`);   
+
+   }
+   return this  //by default it return
+   
 }
+
+const userOne = new User("saif", 12, true)
+const userTwo = new User("javascript", 11, false) // contructor function allow single instance to print values indivisually 
+
+console.log(userOne.constructor); // A constructor propety is reffernce about it self 
+//console.log(userTwo);
+
+
