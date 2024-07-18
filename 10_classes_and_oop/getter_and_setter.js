8d4b68b2ef754f851f30af04013e16cca6a66dc3
Saif Hassan <saifhassan451@gmail.com>
2024-07-18 05:03:22 +0000

    getter and setter

--- 10_classes_and_oop/getter_and_setter.js
+++ 10_classes_and_oop/getter_and_setter.js
@@ -3,7 +3,16 @@ class User {
         this.email = email,
         this. password = password
     }
+   get password(){
+      return this._password.toUpperCase()
+   }
+  
+    set password(value){
+        this._password = value.toUpperCase()
+    }
+
 }
 
-const saif = new User("saif@123", "123")
+const saif = new User("saif@123", "saif")
 console.log(saif.password);
+// 10_classes_and_oop/getter_and_setter.js
