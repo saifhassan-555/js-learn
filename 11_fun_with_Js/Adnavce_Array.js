962ac265366f77113fa0dec01d03cbdc9d6fe6e9
Saif Hassan <saifhassan451@gmail.com>
2024-07-20 11:34:30 +0000

    advance arrays

--- 11_fun_with_Js/Adnavce_Array.js
+++ 11_fun_with_Js/Adnavce_Array.js
@@ -39,4 +39,24 @@ console.log(arrThree[2]); //continues and return
 // SMI(this is the highest optimization) > DOUBLE > PACKED // continues
 // H_SMI > H_DOUBLE > H_PACKED // Holey
 
+const arrFour = new Array(3)
+// just 3 holes. HOLEY_SMI_ELEMENTS
+arrFour[0] = '1' //HOLEY_SMI_ELEMENTS
+arrFour[1] = '2'//HOLEY_ELEMENTS
+arrFour[2] = '2' //HOLEY_ELEMENTS
+
+const arrFive = [] // this is better step for optimization  
+arrFive.push('1') //PACKED_ELEMENTS
+arrFive.push('2') //PACKED_ELEMENTS
+arrFive.push('3') //PACKED_ELEMENTS
+
+
+const arrSix = [1,2,3,4,5]
+
+arrSix.push(Infinity) // it becomes DOUBLE
+
+// for, for-of, forEach use these default method becuase its optimization is much better
+
+
+
 
